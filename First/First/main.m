7a5e043ebecac6b56845ebc3d4e819398c02125a
서효원 <appsleaf@macpro.local>
2017-12-30 23:54:42 +0900

    init

---
+++ First/First/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  First
+//
+//  Created by appsleaf on 2017. 12. 30..
+//  Copyright © 2017년 appsleaf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

0391c5bee2acec6c41cfab549c935fe128c512cb
appsleaf <appsleaf@gmail.com>
2017-12-31 01:12:27 +0900

    comment 추가

--- First/First/Vehicle.h
+++ First/First/Vehicle.h
@@ -15,7 +15,7 @@
 @property int wheels;
 @property int seats;
 
--(void)setWheels:(int)wheels Seats:(int)seats;
+-(void)setWheels:(int)wheels Seats:(int)seats; // 콜론이 기준이고 중간중간 뛰어쓰면 된다.
 -(void)print;
 
 @end
